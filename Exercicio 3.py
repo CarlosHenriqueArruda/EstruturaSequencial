a7ff98ed058548f19616a60c1e5399bbfe404d7b
Carlos Henrique <128426883+CarlosHenriqueArruda@users.noreply.github.com>
2023-04-30 21:36:19 -0300

    Add files via upload

---
+++ Exercicio 3.py
@@ -0,0 +1,6 @@
+Salario = float(input("Quanto você ganha por hora? "))
+Horas = int(input("Quantas horas você trabalhou? "))
+
+Valor = Salario * Horas
+
+print(f'Você trabalhou {Horas} e seu salário será de {Valor}')
