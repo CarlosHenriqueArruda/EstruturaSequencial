2677f25e86a06badf693068ef37f3ede9b33e2c4
Carlos Henrique <carloshenriquearruda82@gmail.com>
2023-05-02 20:05:37 -0300

    Add files via upload

---
+++ Exercicio 5.py
@@ -0,0 +1,10 @@
+Peso = int(input("Peso dos peixes "))
+Multa = 4
+Pesomaximo = Peso - 50
+
+if Peso > 50:
+    print(f"A multa total é {Pesomaximo*Multa}")
+
+else:
+    Peso < 50
+    print(f'Não terá multa')
