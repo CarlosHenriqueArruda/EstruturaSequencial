fd09b27608946573057011a1ad7912a5d42ee039
Carlos Henrique <carloshenriquearruda82@gmail.com>
2023-05-15 00:00:42 -0300

    Delete Exercicio 2.py

--- Estrutura de Decisao/Exercicio 2.py
+++
@@ -1,6 +0,0 @@
-#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-numero = int(input("Digite um numero: "))
-if numero >= 0:
-    print("Esse numero é positivo!")
-else:
-    print("Esse numero é negativo!")
