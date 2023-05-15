5d6a2d374dcdb5986a64647bf61f918591aaa294
Carlos Henrique <carloshenriquearruda82@gmail.com>
2023-05-15 00:00:06 -0300

    Add files via upload

---
+++ Estrutura de Decisao/Exercicio 1.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+numero1 = input("Digite o primeiro numero: ")
+numero2 = input("Digite o segundo numero: ")
+if numero1 > numero2:
+    print("O primeiro numero é o maior!")
+else:
+    print("O segundo numero é o maior!")
