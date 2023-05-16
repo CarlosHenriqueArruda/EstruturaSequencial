fc823edc2deaa8b3e45c191906839ea4ce975a6e
Carlos Henrique <carloshenriquearruda82@gmail.com>
2023-05-16 18:45:20 -0300

    Exercicio 8.py

--- Estrutura de Decisao/Exercicio 8.py
+++ Estrutura de Decisao/Exercicio 8.py
@@ -1,4 +1,16 @@
 #Faça um Programa que leia três números e mostre-os em ordem decrescente.
-numero1 = float(input("Digite o primeiro numero: "))
-numero2 = float(input("Digite o segundo numero: "))
-numero3 = float(input("Digite o terceiro numero: "))
+num1 = int(input("Digite um numero:"))
+num2 = int(input("Digite um numero:"))
+num3 = int(input("Digite um numero:"))
+if num1 > num2 and num1 > num3 and num2 > num3:
+    print(num1, num2, num3)
+elif num1 > num2 and num1 > num3 and num3 > num2:
+    print(num1, num3, num2)
+elif num2 > num3 and num2 > num1 and num3 > num1:
+    print(num2, num3, num1)
+elif num2 > num3 and num2 > num1 and num1 > num3:
+    print(num2, num1, num3)
+elif num3 > num1 and num3>num2 and num2 > num1:
+    print(num3, num2, num1)
+elif num3 > num1 and num3 > num2 and num1 > num2:
+    print(num3, num1, num2)
