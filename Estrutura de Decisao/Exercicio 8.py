237589c0cd81d7dd57871c2fc1220eae49bf00f9
Carlos Henrique <carloshenriquearruda82@gmail.com>
2023-05-16 18:32:55 -0300

    Add files via upload

---
+++ Estrutura de Decisao/Exercicio 8.py
@@ -0,0 +1,4 @@
+#Faça um Programa que leia três números e mostre-os em ordem decrescente.
+numero1 = float(input("Digite o primeiro numero: "))
+numero2 = float(input("Digite o segundo numero: "))
+numero3 = float(input("Digite o terceiro numero: "))
